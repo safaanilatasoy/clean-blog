fc97625b3dfabafd30ade3667e18c946002ab6d4
safaanilatasoy <safaanilatasoy@gmail.com>
2023-10-28 22:34:08 +0300

    main

--- app.js
+++ app.js
@@ -1,17 +1,30 @@
 const express = require('express');
+const ejs = require('ejs');
+const path = require('path');
+
 
 const app = express();
 
+
+// Template Engine
+
+app.set('view engine', 'ejs');
+
+
+//middlewares
+app.use(express.static('public'));
+
+
+// Routes
 app.get('/', (req, res) => {
-  const blog = {
-    id: 1,
-    title: 'Blog title',
-    description: 'Blog description',
-  };
-
-  res.send(blog);
+  res.render('index');
 });
-
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+app.get('/post', (req, res) => {
+  res.render('post');
+});
 const port = 3000;
 
 app.listen(port, () => {
